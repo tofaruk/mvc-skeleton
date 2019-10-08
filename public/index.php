b574e5ea6b135824538fddac9986a1994cb42247
omar.faruk <omar.faruk@mytheresa.com>
2019-10-08 22:36:59 +0200

    added helper

--- public/index.php
+++ public/index.php
@@ -2,16 +2,16 @@
 
 use App\Config\Database;
 use App\Shared\Greeting;
+use App\Shared\Helper;
 
 require __DIR__."/../vendor/autoload.php";
 
 echo '<h2>Index file</h2>';
 $db = Database::getInstance();
 $greeting= new Greeting($db);
-
+$helper = new Helper();
 echo $greeting->welcome("Omar");
 echo '<br>',$greeting->hi("Faruk");
 echo '<br>',$greeting->hello("Asha");
-echo '<pre>';
-print_r($greeting->getAll("Asha"));
-echo '</pre>';
+
+$helper->pd($greeting->getAll("Asha"));
